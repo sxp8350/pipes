d6fcb65afeeac7bb11b3ca98b3b9fb12e40f82fb
arthur <spaul@stuy.edu>
2016-12-15 11:37:23 -0500

    fixed files

---
+++ pipe_networking.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#include "pipe_networking.h"
+
+int main()
+  return 0;
+}
