dd0d99d25813d3a4435fae69eee1a70dcdcb3421
Sebastian Cain <sebastiancain@Sebastians-MacBook-Pro.local>
2016-12-15 11:48:22 -0500

    end of class thurs

--- pipe_networking.c
+++ pipe_networking.c
@@ -4,9 +4,19 @@
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "pipe_networking.h"
 
-int main()
-  return 0;
+int client_handshake(int *buffsize) {
+  
+  return *buffsize;
+}
+
+int server_handshake(int *buffsize) {
+  mkfifo("fifo", 0644);
+  int fd = open("fifo", O_WRONLY);
+
+  return *buffsize;
 }
---
+++ pipe_networking.h
@@ -0,0 +1,3 @@
+int client_handshake(int *buffsize);
+int server_handshake(int *buffsize);
+#define MESSAGE_BUFFER_SIZE 1024
